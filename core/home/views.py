8ae71c9d0b135a5c91eafcf75897b1d502e50e4f
password8475 <manik.goel@progfin.in>
2025-02-27 22:51:01 +0530

    core

---
+++ core/home/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("<h1>Hey! I am a Django Server.</h1>")
+
+
+
+
+def success_page(request):
+    return HttpResponse("<h1>Hey! this is a success page</h1>")
+
+
+
