9af8e6411ed26caedea19dd64c74bad1b3a8c947
password8475 <manik.goel@progfin.in>
2025-02-27 21:46:16 +0530

    Added hello

---
+++ hello/home/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    context = {
+        "variable1" : "rohan is great",
+        "variable2" : "harry is great"
+    }
+    
+    return render(request , 'index.html', context)
+
+def about(request):
+    #return HttpResponse("this is about page")
+    return render(request , 'about.html')
+
+def services(request):
+    #return HttpResponse("this is services page")
+    return render(request , 'services.html')
+
+def contact(request):
+    #return HttpResponse("this is contact page")
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc, date= datetime.today())
+        contact.save()
+        messages.success(request, "Your message has been sent!")
+    return render(request , "contact.html")
